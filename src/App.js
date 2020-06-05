32104f8df56b98fa795a2bf2335e5e2f2336a397
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-05 23:35:59 +0500

    Fetching raw data via axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React from "react";
+import axios from "axios";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component{
+
+    state={advice:""};
+
+    componentDidMount(){
+        // console.log("COmponent did mount");
+        this.fetchAdvice();
+        
+    }
+
+    fetchAdvice=()=>{
+        axios.get("https://api.adviceslip.com/advice")
+
+            .then((response)=>{
+                console.log(response);                
+
+            })
+            .catch((error)=>{
+                console.log(error);
+                
+            })
+    }
+
+    render(){
+        return(
+            <h1>APP</h1>
+        );
+    }
 }
 
 export default App;
